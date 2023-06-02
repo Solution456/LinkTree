08a0e1c9b7c42f2420313545f2995359d9ef1a6f
Solution456 <LegendaryEnOt@bk.ru>
2023-06-02 18:06:58 +0400

    :lipstick: add icons for nav items

--- src/components/layout/Navigation/Navigation.vue
+++ src/components/layout/Navigation/Navigation.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { HomeIcon, LinkIcon, EllipsisHorizontalCircleIcon } from "@heroicons/vue/24/outline";
 
 import styles from "./Navigation.module.scss"
 
@@ -6,19 +7,19 @@ const NAV_ITEMS = [
     {
         path:'/',
         name:'home',
-        icon:0,
+        icon:HomeIcon,
         
     },
     {
         path:'/links',
         name:'links',
-        icon:1,
+        icon:LinkIcon,
         
     },
     {
         path:'/more',
         name:'more',
-        icon:2,
+        icon:EllipsisHorizontalCircleIcon
         
     }
 ]
@@ -30,7 +31,10 @@ const NAV_ITEMS = [
     <nav :class="styles.nav">
         <ul :class="styles.nav_items">
             <li  v-for="route of NAV_ITEMS" :key="route.path">
-                <router-link :class="styles.nav_items__link" :to="route.path">{{ route.name }}</router-link>
+                <router-link :class="styles.nav_items__link" :to="route.path">
+                    <component style="height: 24px; width: 24px;" :is="route.icon"/>
+                    <p class="sub-title-1">{{ route.name }}</p>
+                </router-link>
             </li>
         </ul>
     </nav>
