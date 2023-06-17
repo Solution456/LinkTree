63de228cd6556c33d2bcc5eafd0b515f933d3856
Solution456 <LegendaryEnOt@bk.ru>
2023-06-17 15:49:55 +0400

    💄✨ add button props & add disabl styles

--- src/components/public/Button/Button.vue
+++ src/components/public/Button/Button.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
 import styles from "./Button.module.scss"
+
+type ButtonType = 'button' | 'submit'
+
+interface ButtonProps {
+    type?:ButtonType,
+    disabled?:boolean,
+}
+
+
+const props = withDefaults(defineProps<ButtonProps>(),{
+    type:'button',
+    disabled:false
+})
+
 </script>
 
 <template>
-    <button :class="styles.button">
+    <button :type="props.type" :disabled="disabled" :class="styles.button">
         <div :class="styles.button_content">
             <slot/>
         </div>
